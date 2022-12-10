2e0a5c35a9f2bf481c1a8258260c49dcc8026284
Ninjajkl <ninjajkl@gmail.com>
2022-12-10 13:27:07 -0500

    Added Day 10

---
+++ Day10.py
@@ -0,0 +1,41 @@
+def Part1(inputName):
+    with open("Inputs\\" + inputName + "Input.txt") as f:
+        Input = [line.rstrip("\n").split() for line in f]
+    cycle = 0
+    register = 1
+    signalSum = 0
+    for instr in Input:
+        if instr[0] == "noop":
+            cycle +=1
+            if cycle%40 == 20:
+                signalSum += register*cycle
+        else:
+            for i in range(0,2):
+                cycle += 1
+                if cycle%40 == 20:
+                    signalSum += register*cycle
+                if i == 1:
+                    register += int(instr[1])
+    return signalSum
+
+def Part2(inputName):
+    with open("Inputs\\" + inputName + "Input.txt") as f:
+        Input = [line.rstrip("\n").split() for line in f]
+    cycle = 0
+    register = 1
+    CRT = ""
+    for instr in Input:
+        if instr[0] == "noop":
+            cycle +=1
+            CRT += "#" if len(CRT)%41 in range(register-1, register+2) else "."
+            if cycle%40 == 0:
+                CRT += "\n"
+        else:
+            for i in range(0,2):
+                cycle += 1
+                CRT += "#" if len(CRT)%41 in range(register-1, register+2) else "."
+                if cycle%40 == 0:
+                    CRT += "\n"
+                if i == 1:
+                    register += int(instr[1])
+    return CRT
